428d59ad846772b71a5b0cbb42a34cf9f61eacc7
Christopher <slimcognitodudesvids@outlook.com>
2020-07-26 18:55:12 +1000

    Example initial commit

---
+++ EasyWebsockets.ClientExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Text;
+
+namespace EasyWebsockets.ClientExample
+{
+	class Program
+	{
+		private readonly ClientWebSocket client;
+		private Program()
+		{
+			client = new ClientWebSocket();
+		}
+		static void Main(string[] args)
+		=> new Program().MainAsync().GetAwaiter().GetResult();
+
+		public async Task MainAsync()
+		{
+			await client.ConnectAsync(new Uri("wss://coolwebsocket.tk"), new CancellationToken());
+			Console.WriteLine("connected");
+			while (true)
+			{
+				string t = Console.ReadLine();
+				byte[] b = Encoding.UTF8.GetBytes(t);
+				await client.SendAsync(new ArraySegment<byte>(b), WebSocketMessageType.Text, true, new CancellationToken());
+			}
+		}
+	}
+}
---
+++ ServerExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using EasyWebsockets;
+using EasyWebsockets.Classes;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerExample
+{
+	class Program
+	{
+		private WebSocketServer? server;
+
+		private static void Main()
+		=> new Program().MainAsync().GetAwaiter().GetResult();
+
+		private async Task MainAsync()
+		{
+			server = new WebSocketServer("192.168.1.107", 443, @"C:\Users\User\Desktop\cert.pfx", "asdf");
+
+			server.OnConnect += OnConnect; ;
+			server.OnReceive += OnReceive; ;
+			server.Log += OnLog;
+
+			await server.StartAsync();
+			await Task.Delay(-1);
+		}
+
+		private Task OnLog(IWSLogMessage arg)
+		{
+			Console.WriteLine($"{arg.Process}({arg.Thread}) at {arg.Time}] {arg.Content}");
+			return Task.CompletedTask;
+		}
+
+		private Task OnReceive(WebSocketInstance arg1, byte[] arg2, WSOpcodeType arg3)
+		{
+			if (arg3 == WSOpcodeType.Text)
+			{
+				Console.WriteLine(Encoding.UTF8.GetString(arg2));
+			}
+			return Task.CompletedTask;
+		}
+
+		private Task OnConnect(WebSocketInstance arg)
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
